d863ab8169b1b08d6d6b7ed21bea1732614126a7
DESKTOP-LKAKJAG\haha <2415147256@qq.com>
2022-05-19 17:34:19 +0800

    第二次提交

--- information/common/common-utils/src/main/java/com/lzh/commonutils/JwtUtils.java
+++ information/common/common-utils/src/main/java/com/lzh/commonutils/JwtUtils.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.util.StringUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
@@ -14,82 +15,70 @@ import java.util.Date;
  */
 public class JwtUtils {
 
-    public static final long EXPIRE = 1000 * 60 * 60 * 24;
-    public static final String APP_SECRET = "ukc8BDbRigUDaY6pZFfWus2jZWLPHO";
+  public static final long EXPIRE = 1000 * 60 * 60 * 24;
+  public static final String APP_SECRET = "ukc8BDbRigUDaY6pZFfWus2jZWLPHO";
 
-    /**
-     * 生成token的方法
-     * @param id
-     * @param nickname
-     * @return
-     */
-    public static String getJwtToken(String id, String nickname){
+  /**
+   * 生成token的方法
+   */
+  public static String getJwtToken(String id, String nickname) {
 
-        String JwtToken = Jwts.builder()
-                .setHeaderParam("typ", "JWT")
-                .setHeaderParam("alg", "HS256")  // 固定的
+    // 设置主体部分 可以设置多个值
+    return Jwts.builder()
+        .setHeaderParam("typ", "JWT")
+        .setHeaderParam("alg", "HS256")
+        .setSubject("guli-user")
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
+        .claim("id", id)
+        .claim("nickname", nickname)
+        .signWith(SignatureAlgorithm.HS256, APP_SECRET)
+        .compact();
+  }
 
-                .setSubject("guli-user") // 自定义的值
-                .setIssuedAt(new Date())
-
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
-                .claim("id", id)  // 设置主体部分
-                .claim("nickname", nickname) // 可以设置多个值
-                .signWith(SignatureAlgorithm.HS256, APP_SECRET)
-                .compact();
-
-        return JwtToken;
+  /**
+   * 判断token是否存在与有效
+   */
+  public static boolean checkToken(String jwtToken) {
+    if (StringUtils.isEmpty(jwtToken)) {
+      return false;
     }
-
-    /**
-     * 判断token是否存在与有效
-     * @param jwtToken
-     * @return
-     */
-    public static boolean checkToken(String jwtToken) {
-        if(StringUtils.isEmpty(jwtToken)) {
-            return false;
-        }
-        try {
-            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
+    try {
+      Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * 判断token是否存在与有效
-     * @param request
-     * @return
-     */
-    public static boolean checkToken(HttpServletRequest request) {
-        try {
-            String jwtToken = request.getHeader("token");
-            if(StringUtils.isEmpty(jwtToken)){
-                return false;
-            }
-            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
+  /**
+   * 判断token是否存在与有效
+   */
+  public static boolean checkToken(HttpServletRequest request) {
+    try {
+      String jwtToken = request.getHeader("token");
+      if (StringUtils.isEmpty(jwtToken)) {
+        return false;
+      }
+      Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * 根据token获取会员id
-     * @param request
-     * @return
-     */
-    public static String getMemberIdByJwtToken(HttpServletRequest request) {
-        String jwtToken = request.getHeader("token");
-        if(StringUtils.isEmpty(jwtToken)){
-            return "";
-        }
-        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
-        Claims claims = claimsJws.getBody();
-        return (String)claims.get("id");
+  /**
+   * 根据token获取会员id
+   */
+  public static String getMemberIdByJwtToken(HttpServletRequest request) {
+    String jwtToken = request.getHeader("token");
+    if (StringUtils.isEmpty(jwtToken)) {
+      return "";
     }
+    Jws<Claims> claimsJws = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+    Claims claims = claimsJws.getBody();
+    return (String) claims.get("id");
+  }
 }
